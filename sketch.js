d4f2311a1c9beb291a425b10a89c04b09a20d333
Ridhigutta <70867155+Ridhigutta@users.noreply.github.com>
2020-12-28 19:37:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var dogImg,happyDog,database,foodS,foodStock,dog;
+function preload()
+{
+dogImg=loadImage("images/dogImg.png");
+happyDog=loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  database=firebase.database();
+
+  dog=createSprite(250,250,2,40)
+dog.addImage(dogImg)
+dog.scale=0.25;
+
+foodStock=database.ref("Food")
+foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+background(46,139,87)
+
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS)
+dog.addImage(happyDog);
+}
+
+  drawSprites();
+  fill(0)
+  text("Food Stock:"+foodS,20,20)
+  
+}
+
+function readStock(data){
+foodS=data.val();
+
+}
+
+function writeStock(x){
+  if(x<=0){
+   x=0
+
+  }else{
+  x=x-1
+
+  }
+database.ref("/").update({Food:x})
+}
+
